e2166a206b40733a9b928e5dca89a88a73fe1bce
PRATHAMESH PIMPALE <prathameshpimpale07@gmail.com>
2026-03-31 10:45:50 +0530

    fix mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    console.log("MongoDB Connected ✅");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB Error ❌:", error);git add .
+git commit -m "fix mongo uri"
+git push
     process.exit(1);
   }
 };
